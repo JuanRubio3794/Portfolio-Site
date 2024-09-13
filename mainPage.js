fefcdec359eaa892e4f981046fca2178cd74581d
JuanMRubio <165603377+JuanMRubio@users.noreply.github.com>
2024-09-13 14:21:31 -0700

    New Updates
    Finished contact form functionality, footer section, and animations.

---
+++ mainPage.js
@@ -0,0 +1,66 @@
+	/*Email Script*/
+		const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+				
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+				
+            }
+        })
+        .catch(error => {
+            console.log(error);
+			
+            result.innerHTML = "Something went wrong!";
+			document.getElementById("result").style.color = "#FF0000";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
+
+
+	document.addEventListener("DOMContentLoaded", () => {
+
+	// Use Intersection Observer to determine if objects are within the viewport
+	const observer = new IntersectionObserver(entries => {
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('in-view');
+		  return;
+		}
+		
+	  });
+	});
+
+	// Get all the elements with the .animate class applied
+	const allAnimatedElements = document.querySelectorAll('.animate');
+
+	// Add the observer to each of those elements
+	allAnimatedElements.forEach((element) => observer.observe(element));
+
+});
